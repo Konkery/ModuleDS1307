065f08d3681ee63369d8bba463dcf4e062141d2a
Aleksandr <arrarrat65@gmail.com>
2023-03-13 13:16:33 +0400

    fix

--- js/app/app-test-rtc-1.js
+++ js/app/app-test-rtc-1.js
@@ -6,8 +6,16 @@ const clock_class = require('https://raw.githubusercontent.com/AlexGlgr/ModuleDS
 
 try {
 //    clock = new clock_class.ClassRealTimeClockSet(d,dd,32,fw23,9,9);
-    clock = new clock_class.ClassRealTimeClock(new clock_class.ClassRealTimeClockSet(9, 3, 2023, 12, 0, 15));
-//    let timerId = setInterval(() => print(clock.GetTime()), 1000);
+    clock = new clock_class.ClassRealTimeClock(new clock_class.ClassRealTimeClockSet());
+    let cDate = new Date();
+    cDate.setTime(128743);
+    clock.SetTime(128743);
+    
+    let timerId = setInterval(() => OutTime(), 1000);
+    function OutTime() {
+        console.log(cDate.getTime());
+        console.log(clock.GetTime());
+    }
 } catch(e){
     console.log(`Error>> ${e.Message}, Code>> ${e.Code}`);
 }
