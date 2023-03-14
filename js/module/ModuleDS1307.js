0a86c05bacc057e0ab3a401a8a564242a6b71a41
Aleksandr <arrarrat65@gmail.com>
2023-03-14 14:39:29 +0400

    fix:

--- js/module/ModuleDS1307.js
+++ js/module/ModuleDS1307.js
@@ -120,17 +120,7 @@ class ClassRTCSet {
  * который обеспечивает базовые функции часов
  */
 class ClassRTC {
-    /**
-     * @constructor
-     * @param {Object} _Pin   - - объект класса Pin
-     */
-    constructor(_Pin) {
-        this.name = 'ClassRealTimeClock'; //переопределяем имя типа
-		PrimaryI2C.setup({ sda: SDA, scl: SCL, bitrate: 100000 });
-		this._rtc = require('https://raw.githubusercontent.com/AlexGlgr/ModuleDS1307/fork-Alexander/js/module/rtc.min.js').connect(PrimaryI2C);	
-    }
-	/*******************************************CONST********************************************/
-    /**
+	/**
      * @const
      * @type {number}
      * Константа ERROR_CODE_ARG_VALUE определяет код ошибки, которая может произойти
@@ -145,6 +135,16 @@ class ClassRTC {
      */
 	static get ERROR_MSG_ARG_VALUE() { return `ERROR>> invalid data. ClassID: ${this.name}`; }
     /*******************************************END CONST****************************************/
+    /**
+     * @constructor
+     * @param {Object} _Pin   - - объект класса Pin
+     */
+    constructor(_Pin) {
+        this.name = 'ClassRealTimeClock'; //переопределяем имя типа
+		PrimaryI2C.setup({ sda: SDA, scl: SCL, bitrate: 100000 });
+		this._rtc = require('https://raw.githubusercontent.com/AlexGlgr/ModuleDS1307/fork-Alexander/js/module/rtc.min.js').connect(PrimaryI2C);	
+    }
+	/*******************************************CONST********************************************/
     /**
      * @method
      * 
