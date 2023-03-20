e0604386087e62515693e9b0bf2874d12cbf8982
Aleksandr <arrarrat65@gmail.com>
2023-03-20 10:31:49 +0400

    fix:

--- js/app/app-test-rtc-1.js
+++ js/app/app-test-rtc-1.js
@@ -6,24 +6,35 @@ const clock_class = require('https://raw.githubusercontent.com/AlexGlgr/ModuleDS
 
 try {
 //    clock = new clock_class.ClassRealTimeClockSet(d,dd,32,fw23,9,9);
-    clock = new clock_class.ClassRTC();
+    let clock = new clock_class.ClassRTC();
+
+    clock.SetTime(new Date(
+			2023,
+			4-1,
+			20,
+			10,
+			0,
+			0
+		));
     
-    
+    console.log(clock.GetTimeISO() + ' iso\n');
+
+} catch(e){
+    console.log('Error>> ' + e.Message + ' Code>> ' + e.Code);
+}
     //setTime();
     //const hDate = new Date();
-    //clock.SetTime(hDate);
+    
 
     //console.log(cDate.toISOString() + ' controller');
     //console.log(clock.GetTimeHours() + ' module\n');
     //clock.AdjustTime(11, 'mm');
-    console.log(clock.GetTimeISO() + ' iso\n');
-    console.log(clock.GetTimeHours() + ' hours\n');
-    console.log(clock.GetTimeUnix() + ' unix\n');
+    
     //let timerId = setInterval(() => OutTime(), 1000);
     //function OutTime() {
     //    const cDate = new Date();
         
     //}
-} catch(e){
-    console.log('Error>> ' + e._Message + ' Code>> ' + e._Code);
-}
+//
+//   console.log(clock.GetTimeHours() + ' hours\n');
+//    console.log(clock.GetTimeUnix() + ' unix\n');
